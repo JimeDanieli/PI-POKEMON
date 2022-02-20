b0abcec7d27a4d5573efb437a3f8b93806df3178
Jime <jimedanieli.09@gmail.com>
2022-02-20 20:37:30 -0300

    Types are broken

--- PI-Pokemon-main/client/src/components/PokeCard/PokeCard.jsx
+++ PI-Pokemon-main/client/src/components/PokeCard/PokeCard.jsx
@@ -16,14 +16,14 @@ const PokeCard =({poke}) =>{
                     <h2>{poke.name.charAt(0).toUpperCase()+ poke.name.slice(1)}</h2>
                 </div>
                 {
-                    poke.types &&poke.types.map((t,k) =>{
+                    poke.types && poke.types.map((t,k) =>{
                         return(
                             <div className={styles.icon} key={k}>
                         
                                 <img className={styles.typesImg} src={t.img} alt={t.name}/>
                                 <p className={styles.text}>{t.name.charAt(0).toUpperCase() + t.name.slice(1)}</p>
                             </div>
-                        )
+                        ) 
                     })
                 }
             </div>
